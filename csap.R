439d5d5c314ba582899bf8e4df2f2f3ee9b51068
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-11-10 17:45:41 +0100

    Read precipitation in
    Csapadék beolvasása

---
+++ csap.R
@@ -0,0 +1,22 @@
+.First <- function(){Sys.setenv(TZ='UTC')}
+ttfile <- dir("Csapi")
+
+for(tti in 1:8) {
+    ttaktfile <- ttfile[tti]
+    ttakt <- read.csv2(paste0("Csapi/",ttaktfile), enc="latin1", na="-")
+    ttaktname <- sub(".csv", "", ttaktfile)
+    ttDatejav <- gsub("\\.", "-", ttakt$Date)
+    ttPOSIXdattim <- as.POSIXct(paste(ttDatejav, ttakt$Time))
+    assign(ttaktname, xts(ttakt$ÚjH.mm., ttPOSIXdattim))
+}
+
+for(tti in 9:length(ttfile)) {
+    ttaktfile <- ttfile[tti]
+    ttakt <- read.table(paste0("Csapi/",ttaktfile), na="-", dec=",", sep = "\t", head = TRUE)
+    ttaktname <- sub(".csv", "", ttaktfile)
+    ttDatejav <- gsub("\\.", "-", ttakt$Date)
+    ttPOSIXdattim <- as.POSIXct(paste(ttDatejav, ttakt$Time))
+    assign(ttaktname, xts(ttakt$ÚjH.mm, ttPOSIXdattim))
+}
+
+plot(csap2010, type="h")
