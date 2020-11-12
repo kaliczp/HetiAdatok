760f6b915d06697f8c2daa265bd6c56718d5137c
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-11-12 17:45:02 +0100

    Polish and bind precipitation data and join with temperature.
    Csapadék adatok kiegészítve és összefűzve, hőmérséklettel összerakva.

--- csap.R
+++ csap.R
@@ -27,3 +27,18 @@ sum(csap2017, na.rm=T) #692.71
 
 plot.zoo(csap2017, lwd = 2, type="h")
 lines(as.zoo(oldcsap2017), lwd =2, col="red", type="h")
+
+## Összefűzés
+csap <- c(csap2010, csap2011, csap2012, csap2013, csap2014, csap2015, csap2016, csap2017, csap2018, csap2019, csap2020)
+
+csap.full <- c(csap, xts(rep(0,568), seq(as.POSIXct("2009-12-27")+23.5*60*60,as.POSIXct("2020-11-10")+1, by="weeks")))
+
+## Heti összeg
+csapsum <- apply.weekly(csap.full,function(x)(sum(x, na.rm=TRUE)))
+
+homcsap <- cbind(hom.all,csapsum)
+
+write.zoo(homcsap, "hetihomcsap.csv",sep=";",dec=",")
+
+
+apply.yearly(csap.full,function(x)(sum(x, na.rm=TRUE)))
